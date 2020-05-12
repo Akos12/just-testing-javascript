61ff89efae43b5c6cb94d329bf70cc445ce356c7
Akos12 <64487206+Akos12@users.noreply.github.com>
2020-05-12 15:03:46 +0200

    todo-

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ $('button').click(() => {
     $('ul').append(`<li>${$('input').val()}</li>`);
     $('input').val('');
   });
+  $('#decrease').click(() => {
+    counter--;
+    $('h2').text(counter);
+  });
